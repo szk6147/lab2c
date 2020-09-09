4dcaa347def8dda3403b7e56602e6e756ca0bdce
szk6147 <szk6147@psu.edu>
2020-09-09 01:41:53 +0000

    Added \n

--- main.c
+++ main.c
@@ -17,39 +17,39 @@ int main(void)
 
   if (g >= 93.0)
   {
-    printf("Your letter grade for CMPSC 131 is A.");
+    printf("Your letter grade for CMPSC 131 is A.\n");
   }
   else if (g >= 90.0 && g < 93.0)
   {
-    printf("Your letter grade for CMPSC 131 is A-.");
+    printf("Your letter grade for CMPSC 131 is A-.\n");
   }
   else if (g >= 87.0 && g < 90.0)
   {
-    printf("Your letter grade for CMPSC 131 is B+.");
+    printf("Your letter grade for CMPSC 131 is B+.\n");
   }
   else if (g >= 83.0 && g < 87.0)
   {
-    printf("Your letter grade for CMPSC 131 is B.");
+    printf("Your letter grade for CMPSC 131 is B.\n");
   }
   else if (g >= 80.0 && g < 83.0)
   {
-    printf("Your letter grade for CMPSC 131 is B-.");
+    printf("Your letter grade for CMPSC 131 is B-.\n");
   }
   else if (g >= 77.0 && g < 80.0)
   {
-    printf("Your letter grade for CMPSC 131 is C+.");
+    printf("Your letter grade for CMPSC 131 is C+.\n");
   }
   else if (g >= 70.0 && g < 77.0)
   {
-    printf("Your letter grade for CMPSC 131 is C.");
+    printf("Your letter grade for CMPSC 131 is C.\n");
   }
   else if (g >= 60.0 && g < 70.0)
   {
-    printf("Your letter grade for CMPSC 131 is D.");
+    printf("Your letter grade for CMPSC 131 is D.\n");
   }
   else if (g < 60.0)
   {
-    printf("Your letter grade for CMPSC 131 is F.");
+    printf("Your letter grade for CMPSC 131 is F.\n");
   }
   return 0;
 }
